5b8104e89800decbf77670cb44e3aee5db8776bb
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2024-09-28 00:47:05 +0300

    Create overview layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <div className="">Home/Overview</div>;
+import Card from "./components/Card";
+import { getBalance } from "./lib/data-services";
+
+export default async function Home() {
+  const { current, income, expenses } = await getBalance();
+
+  return (
+    <div className="">
+      <h1 className="mb-10 text-3xl font-bold">Overview</h1>
+
+      <div className="mb-6 flex w-full justify-between gap-4">
+        <Card label="Current Balance" value={current} isPrimary={true} />
+        <Card label="Income" value={income} isPrimary={false} />
+        <Card label="Expenses" value={expenses} isPrimary={false} />
+      </div>
+
+      <div className="grid grid-cols-2 grid-rows-4 gap-5">
+        <div className="rounded-lg bg-white p-10">Pots</div>
+        <div className="row-span-2 rounded-lg bg-white p-10">Budgets</div>
+        <div className="row-span-3 rounded-lg bg-white p-10">Transactions</div>
+        <div className="col-start-2 row-span-2 rounded-lg bg-white p-10">
+          Recurring Bills
+        </div>
+      </div>
+    </div>
+  );
 }
