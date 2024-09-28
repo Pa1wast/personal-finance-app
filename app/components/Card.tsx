2eb776cf9606da3bb778822e41a79c33d650797e
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2024-09-28 18:12:52 +0300

    Add general changes

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -11,14 +11,14 @@ export default function Card({
 }) {
   return (
     <div
-      className={`w-full rounded-xl p-6 ${isPrimary ? "bg-grey-900 text-grey-100" : "bg-white text-grey-900"}`}
+      className={`w-full rounded-xl px-6 py-4 ${isPrimary ? "bg-grey-900 text-grey-100" : "bg-white text-grey-900"}`}
     >
       <h4
-        className={`mb-5 text-sm capitalize ${isPrimary ? "text-grey-100" : "text-grey-500"}`}
+        className={`mb-5 text-xs capitalize ${isPrimary ? "text-grey-100" : "text-grey-500"}`}
       >
         {label}
       </h4>
-      <p className="text-4xl font-bold">{convertToCurrency(value, 2)}</p>
+      <p className="text-2xl font-bold">{convertToCurrency(value, 2)}</p>
     </div>
   );
 }
--- app/components/PotTag.tsx
+++ app/components/PotTag.tsx
@@ -11,12 +11,12 @@ export default function PotTag({
 }) {
   return (
     <div
-      className={`relative flex h-min max-w-xs flex-col pl-4 before:absolute before:left-0 before:h-[90%] before:w-[3px] before:rounded-full before:bg-grey-300 before:content-['']`}
+      className={`relative flex h-min max-w-xs flex-col pl-3 text-xs before:absolute before:left-0 before:h-[90%] before:w-[3px] before:rounded-full before:bg-grey-300 before:content-['']`}
     >
       <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs font-light text-grey-500">
         {name}
       </p>
-      <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-bold">
+      <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs font-bold">
         {convertToCurrency(total)}
       </p>
     </div>
