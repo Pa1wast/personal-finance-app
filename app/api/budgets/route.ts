3118494a2133073e024ccdb205ea01f36545347f
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2024-10-02 22:45:29 +0300

    Update route.ts

--- app/api/budgets/route.ts
+++ app/api/budgets/route.ts
@@ -5,3 +5,8 @@ const { budgets } = data;
 export async function GET() {
   return NextResponse.json(budgets);
 }
+
+export async function POST(request) {
+  console.log(request);
+  return NextResponse.json({ status: 200 });
+}
