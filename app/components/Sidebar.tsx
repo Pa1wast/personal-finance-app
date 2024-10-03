02aa8dec411b87b5d1ce903c28e834793a51d58b
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2024-10-03 04:10:07 +0300

    Update Sidebar.tsx

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,14 +1,27 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 import Logo from "./Logo";
 import MinimizeMenu from "./MinimizeMenu";
 import NavLinks from "./NavLinks";
 import ExpandMenu from "./ExpandMenu";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Sidebar() {
   const [isMinimized, setIsMinimized] = useState(false);
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    const storedValue = localStorage.getItem("isMinimized");
+
+    if (storedValue) setIsMinimized(JSON.parse(storedValue));
+
+    setIsReady(true);
+  }, []);
+
+  useEffect(() => {
+    if (isReady)
+      localStorage.setItem("isMinimized", JSON.stringify(isMinimized));
+  }, [isMinimized, isReady]);
 
   return (
     <div
